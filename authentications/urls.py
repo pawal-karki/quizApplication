e39897aedba10bfc8d9565c886d46fdbb5d4a047
pawal-karki <pawal_karki@icloud.com>
2024-07-17 17:58:39 +0545

    first commit

---
+++ authentications/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from dj_rest_auth.registration.views import RegisterView
+
+urlpatterns = [
+    path('/register', RegisterView.as_view(), name='rest_register'),
+]
