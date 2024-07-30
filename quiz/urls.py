20d1b3e06e8e870a016e381c124423b1e7fd6222
pawal-karki <pawal_karki@icloud.com>
2024-07-30 10:09:52 +0545

    fetching question

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import create_question , get_questions
+from .views import create_questions , get_questions , get_question_by_id
 
 urlpatterns = [
-    path('question/create/',create_question , name='create-question'),
-    path('question/get-questions/', get_questions , name='get-questions')
+    path('questions/create/',create_questions , name='create-question'),
+    path('questions/get-questions/', get_questions , name='get-questions'),
+    path('questions/get-question-by-id/<int:id>', get_question_by_id , name='get-question-by-id')
+
+
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -4,14 +4,16 @@ from rest_framework.decorators import api_view , permission_classes
 from .serializers import QuestionSerializer
 from rest_framework.status import HTTP_201_CREATED , HTTP_400_BAD_REQUEST , HTTP_200_OK
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Question
+# from django.shortcuts import get_object_or_404
 #decorators are the function that takes another function and extends the behavior of the latter function without explicitly modifying it.
 
 
 # Create your views here.
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def create_question(request):
+def create_questions(request):
     serializer = QuestionSerializer(data = request.data)
     if serializer.is_valid():
         serializer.save()
@@ -21,9 +23,22 @@ def create_question(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_questions(request):
-    questions = Question.objects.all()
-    serializer = QuestionSerializer(data=questions)
-    return JsonResponse(serializer.data, status=HTTP_200_OK)
-
+    level = request.query_params.get('level')
     
+    if level:
+        if level not in ['easy', 'medium', 'hard']:
+            return JsonResponse({'error': 'Invalid level specified'}, status=HTTP_400_BAD_REQUEST)
+        questions = Question.objects.filter(level=level)[:20]
+    else:
+        questions = Question.objects.all()[:20]
+
+    serializer = QuestionSerializer(questions, many=True)
+    return Response(serializer.data, status=HTTP_200_OK)
+
+@api_view(['GET'])
+
+def get_question_by_id(request, id):
+    question = Question.objects.filter(pk=id)
+    serializer = QuestionSerializer(instance=question , many=True)
+    return Response(serializer.data, status=HTTP_200_OK)
 
